0fff91be23b42a29c0170925e0b611434062117d
MARCOS VINICIUS DA SILVA ROSA <rosa.3950@aluno.pr.senac.br>
2023-10-02 17:08:25 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/sistemasvendas/SistemasVendas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemasvendas;
+
+/**
+ *
+ * @author rosa.3950
+ */
+public class SistemasVendas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
